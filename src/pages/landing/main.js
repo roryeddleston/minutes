5f5387145d7b58782ef35cc128bc2087251479b3
Rory Eddleston <rory_94@live.co.uk>
2025-02-24 11:44:15 +0000

    added styling to login page

--- src/pages/landing/main.js
+++ src/pages/landing/main.js
@@ -1,14 +1,24 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router-dom';
 import './style.scss';
 import config from './config.json';
-import LoginButton from '../../components/loginButton/main'
+import LoginButton from '../../components/loginButton/main';
 
-export default function Landing(props) {
+export default function Landing() {
+  const { isAuthenticated } = useAuth0();
+  const navigate = useNavigate();
 
-	return (
-		<div className="landing">
-            <h1>{config.landing.heading}</h1>
-			<LoginButton />
-		</div>
-	);
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/home'); // Redirect to home after login
+    }
+  }, [isAuthenticated, navigate]);
+
+  return (
+    <div className="landing">
+      <h1>{config.landing.heading}</h1>
+      <LoginButton />
+    </div>
+  );
 }
